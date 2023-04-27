07e9646d77b957b1734b52526dc614d6fb46567f
Bryan Cortez <bancortez2@gmail.com>
2023-04-27 17:23:09 +0800

    keys routes

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/login',                  'login')->name('admin.login'); // show login form
     Route::get('/admin/keys',                         'keys')->name('admin.keys'); // show all rooms/keys available
     Route::post('/admin/keys',                         'createKey')->name('admin.createKey'); // show all rooms/keys available
-    Route::get('/admin/keys/{id}',                         'key')->name('admin.key'); // show room/key details
     Route::get('/admin/keys/history',                 'history')->name('admin.history'); // show key history
+    Route::get('/admin/keys/{id}',                         'key')->name('admin.key'); // show room/key details
     
     Route::get('/admin/schedules',                 'indexSchedule')->name('admin.createSchedule'); // show all schedule
     Route::get('/admin/schedules/create',                 'createSchedule')->name('admin.createSchedule'); // show schedule form
